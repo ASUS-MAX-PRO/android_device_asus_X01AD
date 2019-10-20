c269e1c4bef4de0a6d401bdd781a9a35b14ada13
Dhina17 <dhinalogu@gmail.com>
2019-10-20 16:19:26 +0000

    X01AD: Build missing audio hidl service and impl from source

--- device.mk
+++ device.mk
@@ -44,7 +44,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl
 
