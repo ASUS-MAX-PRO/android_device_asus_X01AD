b9629ea94c2bbbb506c2a67ec592c7d4e9d18dcc
Nguyenhung9x Developer <35420114+nguyenhung9x2020@users.noreply.github.com>
2021-05-01 16:31:09 +0700

    X01AD: Update BUILD_FINGERPRINT

--- lineage_X01AD.mk
+++ lineage_X01AD.mk
@@ -20,12 +20,21 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Device identifiers.
-PRODUCT_DEVICE := X01AD
 PRODUCT_NAME := lineage_X01AD
+PRODUCT_DEVICE := X01AD
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_X01AD
+PRODUCT_MODEL := Zenfone Max M2
 PRODUCT_MANUFACTURER := asus
-TARGET_VENDOR := asus
-BOARD_VENDOR := asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ASUS_X00T \
+    PRODUCT_NAME=WW_X01AD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8953_64-user 10 WW_Phone-202005071625 17.2018.2004.31-20200507 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "asus/WW_X01AD/ASUS_X01A_1:10/WW_Phone-202005071625/17.2018.2004.31-20200507:user/release-keys"	
