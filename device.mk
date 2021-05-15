94ca3449d38fcbc9140067c0da850ed2ec55f652
Nguyenhung9x Developer <35420114+nguyenhung9x2020@users.noreply.github.com>
2021-05-15 15:55:31 +0700

    Revert "X01AD: Exclude snap overlays from RRO"
    This reverts commit ff8eb710dfd39ed1de531d132f7d1c78c5c9c325.

--- device.mk
+++ device.mk
@@ -17,8 +17,7 @@ PRODUCT_PACKAGES += \
     
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/frameworks/opt/net/wifi \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap
+    $(LOCAL_PATH)/overlay/frameworks/opt/net/wifi
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := X01A,X01AD
